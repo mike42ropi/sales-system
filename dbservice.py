ac968687a558bf5391685f91dafb15fe32f5d13a
mike42ropi <michaelkiprop123@gmail.com>
2023-09-26 12:54:41 -0700

    finale

--- dbservice.py
+++ dbservice.py
@@ -1,4 +1,5 @@
 import psycopg2
+from datetime import datetime
 conn = psycopg2.connect(
     database="myduka_class", user='postgres', password='sequence150')
 def get_data(p):
@@ -18,25 +19,39 @@ def get_data(p):
 
 
 prods=get_data("products")
-print("products is:",prods)
+print(prods)
+
+sale1s=get_data("sales")
+print(sale1s)
+
+
+
+
 
 conn = psycopg2.connect(
      database="myduka_class", user='postgres', password='sequence150')
-def get_data1(s):
-     
 
-     cursor = conn.cursor()
+def add_data1(p,s):
+    conn.autocommit = True
+    cursor = conn.cursor()
+    date1= datetime.now().replace(microsecond=0)
+    columns="(product_name, buying_price, selling_price, stock_quantity)"
+    valuess="('lotion',60,100,10)"
+    columns1="(pid,quantity,created_at)"
+    valuess1=f"(31,2.00,'{date1}')"
+    w =f"INSERT INTO {p} {columns} VALUES {valuess}"
+    l =f"INSERT INTO {s} {columns1} VALUES {valuess1}"
 
-     d="select * from " + " "+s
-     cursor.execute(d)
+    cursor.execute(w)
+    cursor.execute(l)
 
-
-     data1 = cursor.fetchall()
+conn.commit()
+ 
     
     
-     return data1
-     
+ 
+bwx=add_data1("products","sales")
 
-sale1s=get_data1("sales")
-print("sales is:",sale1s)
+# bwx=add_data1()
 conn.close()
+
