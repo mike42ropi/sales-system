19db151fdf7e86fbc282eae56df7721f71311b49
mike42ropi <michaelkiprop123@gmail.com>
2023-10-03 16:51:54 -0700

    update1

--- dbservice.py
+++ dbservice.py
@@ -1,5 +1,5 @@
 import psycopg2
-from datetime import datetime
+
 
 conn = psycopg2.connect(
     database="myduka_class", user='postgres', password='sequence150')
@@ -15,10 +15,7 @@ class myduka_:
         self.sales=sales
         self.get_data()
         self.get_data1()
-        self.add_data1()
-        self.add_data2()
-        self.rem_stock()
-        
+     
     
     def get_data(self):
         
@@ -48,57 +45,6 @@ class myduka_:
         return self.data2 
 
 
-
-    def add_data1(self):
-            
-        cursor = conn.cursor()
-        
-        columns = "(product_name,buying_price,selling_price,stock_quantity)"
-        valuess = f"{vp}"
-        
-        w = f"INSERT INTO {self.products} {columns} VALUES {valuess}"
-
-
-        cursor.execute(w)
-        conn.commit()
-
-              
-
-    def add_data2(self):
-          
-        cursor = conn.cursor()
-        date1 = datetime.now().replace(microsecond=0)
-   
-        columns1 = "(quantity,created_at)"
-        valuess1 = f"({vs},'{date1}')"
- 
-        l = f"INSERT INTO {self.sales} {columns1} VALUES {valuess1}"
-
-   
-        cursor.execute(l)
-        conn.commit()
-
-
-
-    def rem_stock(self):
-        cursor = conn.cursor()
-        m =f"SELECT {self.products}.product_name,SUM({self.products}.stock_quantity-{self.sales}.quantity)\
-        AS rem_stock from {self.sales}  JOIN products  ON {self.sales}.pid = {self.products}.id GROUP BY\
-        {self.products}.product_name ORDER BY rem_stock"
-
-        cursor.execute(m)
-
-
-        self.data3=cursor.fetchall()
-        return self.data3
-        
-
-vp =  (input("Enter Product name: "),
-       float(input("Enter buying_price: ")), 
-       float(input("Enter selling_price: ")),
-       float(input("Enter Stock quantity: ")))
-
-vs = float(input("Enter a quantity: "))
 
 prosa=myduka_("products","sales")
 
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import psycopg2
+conn = psycopg2.connect(
+    database="myduka_class", user='postgres', password='sequence150')
 from flask import Flask, render_template
 from dbservice import myduka_
+from flask import request, redirect
 
 app=Flask(__name__)
 
@@ -12,13 +16,28 @@ def dashboard():
     return render_template("dashboard.html")
 
 
-@app.route("/products")
+@app.route("/products", methods=['GET', 'POST'])
 def products():
     p = myduka_("products","sales")
     sp = p.get_data()
+    if request.method == "POST":
+           cursor = conn.cursor()
+           prod_n= request.form['product_name']
+           buy_p= request.form['buying_price']
+           sell_p= request.form['selling_price']
+           stock_q= request.form['stock_quantity'] 
+           columns = "(product_name,buying_price,selling_price,stock_quantity)"
+           valuess=(prod_n,buy_p,sell_p,stock_q)
+           w = f"INSERT INTO products {columns} VALUES {valuess}"
+           cursor.execute(w)
+           conn.commit()
+           cursor.close
+    
     return render_template("products.html", myprods = sp)
 
 @app.route("/sales")
 def sales():
-    return render_template("sales.html")
+    s = myduka_("products","sales")
+    sp1 = s.get_data1()
+    return render_template("sales.html", myprods1 = sp1)
 app.run()
